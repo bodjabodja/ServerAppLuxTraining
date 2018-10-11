038890dcfd969feb6e209c7afff9dba430d7052c
dsk16 <test@email.com>
2018-10-11 17:58:27 +0300

    Server.java - добавлено описание

--- src/com/net/Server.java
+++ src/com/net/Server.java
@@ -8,6 +8,8 @@ import java.net.Socket;
  * Created by dsk16 on 10/11/2018.
  */
 public class Server {
+
+    //TODO: like - -- https://gist.github.com/Rooman/ccf78918f884ae354580cf9ebd70142b
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(3000);
         Socket socket = serverSocket.accept();
@@ -18,11 +20,12 @@ public class Server {
             while (!(message = socketBufferedReader.readLine()).isEmpty()) {
                 System.out.println(message);
             }
-
+            File file = new File(message);//идем по пути к файлу
+            //вычитываем и отдаем на вне(например через стринг билдер
             System.out.println("sending response:");
             socketBufferedWriter.write("HTTP/1.1 200 OK\n");
             socketBufferedWriter.write("\n");
-            socketBufferedWriter.write("Hello browser!");
+            socketBufferedWriter.write("webapp");
         }
     }
 }

9a1ea689ad4ba19aa1a821d3d6e6625b44b0e308
dsk16 <test@email.com>
2018-10-12 10:20:56 +0300

    Server.java - добавлено описание

--- src/com/net/Server.java
+++ src/com/net/Server.java
@@ -29,32 +29,35 @@ public class Server {
         }
     }
 
+    //todo: implement func to send and view style.css
     public void start() throws IOException {
         ServerSocket serverSocket = new ServerSocket(port);
         Socket socket = serverSocket.accept();
         try (BufferedReader socketBufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              BufferedWriter socketBufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));) {
 
-            String message;
-            while (!(message = socketBufferedReader.readLine()).isEmpty()) {
-                System.out.println(message);
-            }
+            //while (true) {
+                String message;
+                while (!(message = socketBufferedReader.readLine()).isEmpty()) {
+                    System.out.println(message);
+                }
 
-            File file = new File("C:\\IdeaProjects\\ServerAppLuxTraining\\src\\com\\net\\webapp\\" + message);//идем по пути к файлу
-            //вычитываем и отдаем на вне(например через стринг билдер
-            System.out.println("sending response:");
-            socketBufferedWriter.write("HTTP/1.1 200 OK\n");
-            socketBufferedWriter.write("\n");
-            socketBufferedWriter.write(content.toString());
+                //File file = new File("P:\\Users\\dsk16\\IdeaProjects\\ServerAppLuxTraining\\src\\com\\net\\webapp\\" + message);//идем по пути к файлу
+                //вычитываем и отдаем на вне(например через стринг билдер
+                System.out.println("sending response:");
+                socketBufferedWriter.write("HTTP/1.1 200 OK\n");
+                socketBufferedWriter.write("\n");
+                socketBufferedWriter.write(content.toString());
+            //}
         }
     }
 
-    //TODO: like - -- https://gist.github.com/Rooman/ccf78918f884ae354580cf9ebd70142b
+
     public static void main(String[] args) throws IOException {
 
             Server server = new Server();
             server.setPort(3000);
-            server.setWebAppPath("C:\\IdeaProjects\\ServerAppLuxTraining\\src\\com\\net\\webapp\\index.html");
+            server.setWebAppPath("P:\\Users\\dsk16\\IdeaProjects\\ServerAppLuxTraining\\src\\com\\net\\webapp\\index.html");
             server.start();
     }
 }
